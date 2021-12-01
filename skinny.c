fcd24966bf9423caccb43409648b7086b330f52f
rikuanttila <riku.anttila0412@gmail.com>
2021-12-01 21:35:40 +0200

    Comments added

--- skinny.c
+++ skinny.c
@@ -4,15 +4,9 @@
 /**
  * SKINNY-128-384 block cipher encryption.
  * Under 48-byte tweakey at k, encrypt 16-byte plaintext at p and store the 16-byte output at c.
- * 
- * Rakenne:
- * 1.intial state
- * 2.SubCells 8-bit s-box
- * 3.AddConstants Layer
- * 4.ART Add Round Tweaky
- * 5.ShiftRows
- * 6.MixColumns
- * 
+ * Names: Riku Anttila & Juhani Sankari 
+ * Program that encrypts given char array using the SKINNY-128-384 block cipher
+ * encryption algorithm.
  */
 
 uint8_t S8 [16][16] = {
@@ -212,6 +206,3 @@ void skinny(unsigned char *c, const unsigned char *p, const unsigned char *k) {
     
 
 }
-
-
-A
